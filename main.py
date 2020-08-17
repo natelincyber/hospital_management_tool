238a184d4ef3304a50ec7b178b005c7c0c05aa37
real-myth <mithilsmail@gmail.com>
2020-08-16 17:10:13 -0700

    new file:   Templates/main_page.html 	modified:   main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def main_page():
-    return 'Hi'
+    return render_template("main_page.html")
 
 if __name__ == '__main__':
     app.run()
