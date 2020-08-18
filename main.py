0bfbf52759e1245cd60925cad6cb9b644f65fad3
real-myth <mithilsmail@gmail.com>
2020-08-17 17:38:40 -0700

    new file:   Templates/add_patient.html 	new file:   Templates/base.html 	new file:   Templates/index.html 	deleted:    Templates/main_page.html 	new file:   Templates/see_patients.html 	modified:   main.py 	new file:   static/example_logo.png 	new file:   static/logo_for_now.png 	new file:   static/temp_logo.png

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def main_page():
-    return render_template("main_page.html")
+def index():
+    return render_template("index.html")
+
+@app.route("/add_patient")
+def add_patient():
+    return render_template("add_patient.html")
+
+@app.route("/see_patients")
+def see_patient():
+    return render_template("see_patients.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
