a45292739c045d40369ae75da52174ed3260274a
real-myth <mithilsmail@gmail.com>
2020-08-20 13:08:25 -0700

    modified:   Templates/edit.html 	modified:   main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from werkzeug.utils import redirect
 
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///patients.db'
